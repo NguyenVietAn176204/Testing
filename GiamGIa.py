1ffcc73bd94886d171d55895259aaa7dd09a6909
NguyenVietAn176204 <annguyenviet176204@gmail.com>
2024-03-27 17:30:01 +0700

    code tuan 3+4

---
+++ GiamGIa.py
@@ -0,0 +1,95 @@
+import unittest
+def GiamGia(a , b):
+    if a < 0 or b < 0:
+        return -1
+    if a <= 120 and b <= 6:
+        return 100
+    if a >120 and b >= 6:
+        return 0
+    if a <= 120 and b > 6:
+        return 50
+    if a > 120 and b <= 6:
+        return 70
+    
+class GiamGiaTestBien(unittest.TestCase):
+    def test_giam_gia_1(self):
+        result = GiamGia(119,7)
+        self.assertEqual(result, 50)
+
+    def test_giam_gia_2(self):
+        result = GiamGia(119,0)
+        self.assertEqual(result, 100)
+
+    def test_giam_gia_3(self):
+        result = GiamGia(119,1)
+        self.assertEqual(result, 100)
+
+    def test_giam_gia_4(self):
+        result = GiamGia(119,9)
+        self.assertEqual(result, 50)
+
+    def test_giam_gia_5(self):
+        result = GiamGia(119, 100)
+        self.assertEqual(result, 50)
+
+    def test_giam_gia_5(self):
+        result = GiamGia(30,7)
+        self.assertEqual(result, 50)
+
+    def test_giam_gia_5(self):
+        result = GiamGia(31, 7)
+        self.assertEqual(result, 50)
+
+    def test_giam_gia_5(self):
+        result = GiamGia(199,7)
+        self.assertEqual(result, 0)
+
+    def test_giam_gia_5(self):
+        result = GiamGia(200,7)
+        self.assertEqual(result, 0)
+
+
+class GiamGiaTestBangQD(unittest.TestCase):
+    def test_giam_gia_1(self):
+        result = GiamGia(110,5)
+        self.assertEqual(result, 100)
+
+    def test_giam_gia_2(self):
+        result = GiamGia(130,5)
+        self.assertEqual(result, 70)
+
+    def test_giam_gia_3(self):
+        result = GiamGia(110,7)
+        self.assertEqual(result, 50)
+
+    def test_giam_gia_4(self):
+        result = GiamGia(150,9)
+        self.assertEqual(result, 0)
+
+    def test_giam_gia_5(self):
+        result = GiamGia(-1, -1)
+        self.assertEqual(result, -1)
+
+
+class GiamGiaTestC2(unittest.TestCase):
+    def test_giam_gia_1(self):
+        result = GiamGia(110,5)
+        self.assertEqual(result, 100)
+
+    def test_giam_gia_2(self):
+        result = GiamGia(110,7)
+        self.assertEqual(result, 50)
+        
+    def test_giam_gia_3(self):
+        result = GiamGia(122,5)
+        self.assertEqual(result, 70)
+
+    def test_giam_gia_4(self):
+        result = GiamGia(121,8)
+        self.assertEqual(result, 0)
+    def test_giam_gia_5(self):
+        result = GiamGia(122,9)
+        self.assertEqual(result, 0)
+if __name__ =='__main__':
+    unittest.main()
+
